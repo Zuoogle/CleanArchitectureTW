6ea5e8254a23d69f19cf37acda300f9b587e4909
Sherefedin M Getahun <smgetahun@live.com>
2022-04-17 19:11:44 -0700

    Adding Leave Management Applicaiton Layer with Presitance folder and Contracts Folder with necessary Repsository Interfaces

---
+++ HRLeaveManagement/HRLeaveManagement.Application/Persistence/Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,11 @@
+﻿using LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
+    {
+    }
+}
---
+++ HRLeaveManagement/HRLeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository:IGenericRepository<LeaveRequest>
+    {
+
+    }
+}
