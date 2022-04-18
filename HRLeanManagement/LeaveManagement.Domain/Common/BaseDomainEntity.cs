70b1fe7f7325698065d7028924e3ec55f5062d89
Sherefedin M Getahun <smgetahun@live.com>
2022-04-17 18:46:35 -0700

    Finised Adding Basic Domain Entity Classes
    LeaveType, LeaveRequest, and LeaveAllocation

---
+++ HRLeanManagement/LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Domain.Common;
+public class BaseDomainEntity
+{
+    public int Id { get; set; }
+    public DateTime DateCreated { get; set; }
+    public string CreatedBy { get; set; } = String.Empty;
+    public DateTime LastModifiedDate { get; set; }
+    public string LastModifiedBy { get; set; } = String.Empty;
+}
---
+++ HRLeanManagement/LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿using LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Domain;
+public class LeaveType : BaseDomainEntity
+{
+    public string Name { get; set; } = String.Empty;
+    public int DefaultDays { get; set; }
+}
